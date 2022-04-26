34386805787da37d5e20bbb779b97c81efbcf3f4
Kudinov Ivan <102667861+ikudin0v@users.noreply.github.com>
2022-04-27 00:50:02 +0300

    code refactoring
    Full code refactoring.
    Realize all functional via function declarations.

--- index.js
+++ index.js
@@ -1,121 +1,82 @@
-const currentTasks = [0]
-const completedTasks = []
+let currentTasks = [0]
+let completedTasks = []
+
+function addFn() {
+    if (document.getElementById('addText').value != '') {
+        const taskId = currentTasks[0]
+        document.querySelector('#currentTasks').insertAdjacentHTML("afterend", `<div class="taskBox" id="${'taskBoxId' + taskId}">
+                                                        <div class="task checker"><img src="img/done.png" alt="" id="${'doneTask' + taskId}" class="doneTaskBtn"></div> 
+                                                        <div class="task taskText" id="${'textTask' + taskId}"> ${document.getElementById('addText').value} </div>
+                                                        <div class="task edit"><img src = "/img/edit.png",  id="${'editTask' + taskId}" class="editTaskBtn"></div>
+                                                        <div class="task"><img src="/img/delete.png" alt="" id="${'deleteTask' + taskId}" class="delTaskBtn"></div>
+                                                    </div>`)
+
+        document.getElementById('doneTask' + taskId).addEventListener('click', () => {doneFn(taskId)})
+        document.getElementById('editTask' + taskId).addEventListener('click', () => {editFn(taskId)})
+        document.getElementById('deleteTask' + taskId).addEventListener('click', () => {deleteFn(taskId)})
+        currentTasks.push([currentTasks[0], document.getElementById('addText').value])
+        currentTasks[0]++
+        document.getElementById('addText').value = ''
+    }
+}
+
+function doneFn(taskId) {
+    document.querySelector('#completedTasks').insertAdjacentHTML("afterend", `<div class="completedTaskBox" id="${'taskBoxId' + taskId}"> <div></div> 
+                                            <div class="task taskText" id="${'doneTextTask' + taskId}"> 
+                                            ${document.getElementById('textTask' + taskId).textContent}</div> <div></div>
+                                            <div class="task delete"><img src="/img/delete.png" alt="" id="${'deleteCompletedTask' + taskId}" class="delTaskBtn"></div>
+                                        </div>`)
+    document.getElementById(`taskBoxId${taskId}`).remove()
+
+    //добавление элемента в массив completed тасков и удаление его из curent тасков
+    for (i = 1; i <= currentTasks.length - 1; i++){
+        if (currentTasks[i][0] == taskId) {
+            completedTasks.push(currentTasks[i])
+            currentTasks.splice(i, 1)
+        }
+    }   
+
+    document.getElementById('deleteCompletedTask' + taskId).addEventListener('click', () => {deleteCompFn(taskId)})
+}
+
+function deleteCompFn(taskId) {
+    document.getElementById('taskBoxId' + taskId).remove()
+
+    //удаление элемента из массива комплетед тасков
+    for (i = 0; i <= completedTasks.length - 1; i++){
+
+        if (completedTasks[i][0] == taskId) {
+            completedTasks.splice(i, 1)
+        }
+    }
+}
+
+function editFn(taskId) {
+    let newTaskText = prompt('Edit Task', document.getElementById('editTask' + taskId).textContent)
+
+    if (newTaskText != '' || newTaskText != null) {
+        document.getElementById('textTask' + taskId).textContent = newTaskText
+
+        //изменение элемента в массиве курент тасков
+        for (i = 1; i <= currentTasks.length - 1; i++){
+            if (currentTasks[i][0] == taskId) {
+                currentTasks[i][1] = newTaskText
+            }
+        }
+    }
+}
+
+function deleteFn(taskId) {
+    document.getElementById('taskBoxId' + taskId).remove()
+
+    //удаление элемента из массива курент тасков
+    for (i = 1; i <= currentTasks.length - 1; i++){
+        if (currentTasks[i][0] == taskId) {
+            currentTasks.splice(i, 1)
+        }
+    }
+}
 
 document.querySelector('.addButton').onclick = () => {
-
-    if (document.getElementById('addText').value != '') {
-        
-        const index = []
-        index.length = 0
-        index.push(currentTasks[0])
-        index.push(document.getElementById('addText').value)
-
-        document.querySelector('#currentTasks').insertAdjacentHTML("afterend", `<div class="taskBox" id="${'taskBoxId' + index[0]}">
-                                                        <div class="task checker"><img src="img/done.png" alt="" id="${'doneTask' + index[0]}" class="doneTaskBtn"></div> 
-                                                        <div class="task taskText" id="${'textTask' + index[0]}"> ${document.getElementById('addText').value} </div>
-                                                        <div class="task edit"><img src = "/img/edit.png",  id="${'editTask' + index[0]}" class="editTaskBtn"></div>
-                                                        <div class="task"><img src="/img/delete.png" alt="" id="${'deleteTask' + index[0]}" class="delTaskBtn"></div>
-                                                    </div>`)
-
-        currentTasks.push(index)
-        currentTasks[0] += 1
-        console.log(currentTasks)
-        document.getElementById('addText').value = ''
-
-        // вешаем event listener на done таски
-        document.addEventListener('click', function(e) {
-            let doneId = 'doneTask' + index[0]
-            if (e.target.id == doneId || e.target.closest("#doneId")) {
-                //do something
-                document.querySelector('#completedTasks').insertAdjacentHTML("afterend", `<div class="completedTaskBox" id="${'taskBoxId' + e.target.id.replace('doneTask', '')}"> <div></div> 
-                                                        <div class="task taskText" id="${'doneTextTask' + e.target.id.replace('doneTask', '')}"> 
-                                                        ${document.getElementById(`textTask${e.target.id.replace('doneTask', '')}`).textContent}</div><div></div>
-                                                        <div class="task delete"><img src="/img/delete.png" alt="" id="${'deleteCompletedTask' + e.target.id.replace('doneTask', '')}" class="delTaskBtn"></div>
-                                                    </div>`)
-                document.getElementById(`taskBoxId${e.target.id.replace('doneTask', '')}`).remove()
-
-                //добавление элемента в массив completed тасков и удаление его из curent тасков
-                for (i = 1; i <= currentTasks.length - 1; i++){
-                    if (currentTasks[i][0] == e.target.id.replace('doneTask', '')) {
-                        completedTasks.push(currentTasks[i])
-                        currentTasks.splice(i, 1)
-                        console.log(completedTasks)
-                        console.log(currentTasks)
-                    }
-                }   
-                // вешаем event listener на delete completed таски
-                document.addEventListener('click', function(c) {
-                    let deleteId = 'deleteCompletedTask' + e.target.id.replace('doneTask', '')
-                    if (c.target.id == deleteId || c.target.closest("#deleteId")) {
-                        //do something
-                        let deletingTask = 'taskBoxId' + c.target.id.replace('deleteCompletedTask', '')
-                        document.getElementById(deletingTask).remove()
-
-                        //удаление элемента из массива комплетед тасков
-                        for (i = 0; i <= completedTasks.length - 1; i++){
-
-                            if (completedTasks[i][0] == c.target.id.replace('deleteCompletedTask', '')) {
-                                completedTasks.splice(i, 1)
-                                console.log(completedTasks)
-                            }
-                        }
-                    }
-                })
-            }
-        })
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        // вешаем event listener на edit таски
-        document.addEventListener('click', function(e) {
-            let editId = 'editTask' + index[0]
-            let editedTextId = 'textTask' + index[0]
-            if (e.target.id == editId || e.target.closest("#editId")) {
-                //do something
-                let newTaskText = prompt('Edit Task', document.getElementById(editedTextId).textContent)
-
-                if (newTaskText != '' || newTaskText != null) {
-                    document.getElementById(editedTextId).textContent = newTaskText
-
-                    //изменение элемента в массиве курент тасков
-                    for (i = 1; i <= currentTasks.length - 1; i++){
-                        let editListId = e.target.id
-
-                        if (currentTasks[i][0] == editListId.replace('editTask', '')) {
-                            currentTasks[i][1] = newTaskText
-                            console.log(currentTasks)
-                        }
-                    }
-                }
-           }
-        })
-
-        // вешаем event listener на delete таски
-        document.addEventListener('click', function(e) {
-            let deleteId = 'deleteTask' + index[0]
-            let deletingTask = 'taskBoxId' + index[0]
-            if (e.target.id == deleteId || e.target.closest("#deleteId")) {
-                //do something
-                document.getElementById(deletingTask).remove()
-
-                //удаление элемента из массива курент тасков
-                for (i = 1; i <= currentTasks.length - 1; i++){
-
-                    if (currentTasks[i][0] == e.target.id.replace('deleteTask', '')) {
-                        currentTasks.splice(i, 1)
-                        console.log(currentTasks)
-                    }
-                }
-           }
-        })
-
+    addFn()
 }
-}
-    
